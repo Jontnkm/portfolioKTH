7ac73dd5fb0061417bcd1a1e4a8d96b28c6070c0
Jonathan Kim <101095622+Jontnkm@users.noreply.github.com>
2026-04-08 17:19:42 +0900

    2026.04.08 pf update

--- project/src/common/js/gsap.js
+++ project/src/common/js/gsap.js
@@ -105,7 +105,7 @@ if (lastSection) {
 }
 
 // 갤러리 리빌 효과도 반응형 start 지점 조절 (모바일 고려)
-gsap.utils.toArray('.demo-gallery li').forEach((li) => {
+gsap.utils.toArray('.gallery li').forEach((li) => {
     ScrollTrigger.create({
         trigger: li,
         start: "top 50%", 
@@ -156,3 +156,55 @@ const initBgColorChange = () => {
 initBgColorChange();
 
 imagesLoaded(images).on('progress', updateProgress).on('always', showDemo);
+
+/*------------------------------
+Register plugins
+------------------------------*/
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+const content = document.querySelector('.allWrapper')
+
+/*------------------------------
+Making some circles noise
+------------------------------*/
+const simplex = new SimplexNoise()
+for (let i = 0; i < 6000; i++) {
+  const div = document.createElement('div')
+  div.classList.add('circle')
+  const n1 = simplex.noise2D(i * 0.003, i * 0.0033)
+  const n2 = simplex.noise2D(i * 0.002, i * 0.001)
+  
+  const style = {
+    transform: `translate(${n2 * 200}px) rotate(${n2 * 270}deg) scale(${3 + n1 * 2}, ${3 + n2 * 2})`,
+    boxShadow: `0 0 0 .2px hsla(${Math.floor(i*0.3)}, 70%, 70%, .6)`,
+    top: i + "px"
+  }
+  Object.assign(div.style, style)
+  content.appendChild(div)
+}
+const Circles = document.querySelectorAll('.circle')
+
+/*------------------------------
+Init ScrollSmoother
+------------------------------*/
+const scrollerSmoother = ScrollSmoother.create({
+  content: content,
+  wrapper: '.layoutContent',
+  smooth: 1,
+  effects: false
+});
+
+/*------------------------------
+Scroll Trigger
+------------------------------*/
+const main = gsap.timeline({
+  scrollTrigger: {
+    scrub: .7,
+    start: "top 75%",
+    end: "bottom bottom"
+  }
+})
+Circles.forEach((circle) => {
+  main.to(circle, {
+    opacity: 1,
+  })
+})
